752f23fd453358b5a3bf44e022709fb7b08b9eb6
thanuja <thanujakularathna0606@gmail.com>
2026-09-02 15:35:33 +0530

    fix reviews not showing issue

--- src/features/tours/pages/TourDetailPage.jsx
+++ src/features/tours/pages/TourDetailPage.jsx
@@ -1,17 +1,17 @@
-import { lazy, Suspense } from 'react';
-import { useParams } from 'react-router-dom';
-import { useTour } from '../hooks/useTour';
-import TourHero from '../components/TourHero';
-import TourDescription from '../components/TourDescription';
-import TourPictures from '../components/TourPictures';
-import TourReviews from '../components/TourReviews';
-import TourCTA from '../components/TourCTA';
-import Spinner from '../../../components/Spinner/Spinner';
-import ErrorState from '../../../components/ErrorState/ErrorState';
-import Header from '../../../components/Header/Header';
-import Footer from '../../../components/Footer/Footer';
+import { lazy, Suspense } from "react";
+import { useParams } from "react-router-dom";
+import { useTour } from "../hooks/useTour";
+import TourHero from "../components/TourHero";
+import TourDescription from "../components/TourDescription";
+import TourPictures from "../components/TourPictures";
+import TourReviews from "../components/TourReviews";
+import TourCTA from "../components/TourCTA";
+import Spinner from "../../../components/Spinner/Spinner";
+import ErrorState from "../../../components/ErrorState/ErrorState";
+import Header from "../../../components/Header/Header";
+import Footer from "../../../components/Footer/Footer";
 
-const TourMap = lazy(() => import('../components/TourMap'));
+const TourMap = lazy(() => import("../components/TourMap"));
 
 export default function TourDetailPage() {
   const { slug } = useParams();
@@ -19,6 +19,7 @@ export default function TourDetailPage() {
 
   if (isLoading) return <Spinner fullPage />;
   if (isError) return <ErrorState message={error.message} />;
+  console.log(tour.reviews);
 
   return (
     <>
