00d74b51c31ea9f44ef2b46d6562c5cd84c24946
thanuja <thanujakularathna0606@gmail.com>
2026-09-16 21:33:30 +0530

    edit vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_GATEWAY_URL || 'http://localhost:3000',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
